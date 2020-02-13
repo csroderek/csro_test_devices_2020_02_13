a3ce37ac5474b0b05541d28bc3f4bd9e2eec7463
dereklitaoo <dereklitao@gmail.com>
2020-02-13 23:16:00 +0800

    nlight works ok

--- csro_nlight.py
+++ csro_nlight.py
@@ -11,21 +11,20 @@ class Virtual_CSRO_Nlight(threading.Thread):
         self.channels = num
         self.client = mqtt.Client()
         self.nlight_status = [0, 0, 0, 0]
-        
 
     def on_connect(self, client, userdata, flags, rc):
-        # self.client.subscribe('csro/'+self.mac+"/" +
-        #                       self.device_type+"/set/#", qos=0)
-        for i in range(0, self.channels):
+        self.client.subscribe('csro/'+self.mac+"/" +
+                              self.device_type+"/set/#", qos=0)
+        for dev_index in range(0, self.channels):
             device = {
                 "~": "csro/" + self.mac + "/" + self.device_type,
-                "name": self.mac + "_"+self.device_type+"_"+str(i),
-                "unique_id": self.mac + "_"+self.device_type+"_"+str(i),
-                "cmd_t": "~/set/" + str(i),
+                "name": self.mac + "_"+self.device_type+"_"+str(dev_index),
+                "uniq_id": self.mac + "_"+self.device_type+"_"+str(dev_index),
+                "cmd_t": "~/set/" + str(dev_index),
                 "pl_on": 1,
                 "pl_off": 0,
                 "stat_t": "~/state",
-                "stat_val_tpl": "{{value_json.state["+str(i)+"]}}",
+                "stat_val_tpl": "{{value_json.state["+str(dev_index)+"]}}",
                 "avty_t": "~/available",
                 "dev": {
                     "ids": self.mac + "_"+self.device_type,
@@ -36,15 +35,31 @@ class Virtual_CSRO_Nlight(threading.Thread):
                 }
             }
             json_message = json.dumps(device)
-            self.client.publish("derek/test/", json_message)
-            print(json_message)
+            self.client.publish("csro/light/"+self.mac+"_"+self.device_type +
+                                "_"+str(dev_index)+"/config", json_message)
+        self.client.publish("csro/"+self.mac+"/" +
+                            self.device_type+"/available", "online")
+        self.update_status()
 
     def on_message(self, client, userdata, msg):
-        print(msg.topic + " " + str(msg.payload))
+        update = False
+        for dev_index in range(0, self.channels):
+            if str(msg.topic) == str("csro/" + self.mac + "/" + self.device_type+"/set/"+str(dev_index)):
+                if msg.payload.decode('UTF-8') == "0":
+                    self.nlight_status[dev_index] = 0
+                elif msg.payload.decode('UTF-8') == "1":
+                    self.nlight_status[dev_index] = 1
+                update = True
+            else:
+                pass
+        if update == True:
+            self.update_status()
 
     def run(self):
         self.client.on_connect = self.on_connect
         self.client.on_message = self.on_message
+        self.client.will_set("csro/"+self.mac+"/" +
+                             self.device_type+"/available", "offline")
         self.client.connect('csro.net.cn', 1883, 60)
         self.client.loop_forever()
 
@@ -52,14 +67,15 @@ class Virtual_CSRO_Nlight(threading.Thread):
         state = {
             "state": self.nlight_status
         }
-        json_message = json.dumps(state)
-        self.client.publish()
+        state_message = json.dumps(state)
+        self.client.publish("csro/"+self.mac+"/" +
+                            self.device_type+"/state", state_message)
 
 
 if __name__ == '__main__':
-    nlight1 = Virtual_CSRO_Nlight("abababcdcdcd", "nlight_nb_4k4r", 2)
+    nlight1 = Virtual_CSRO_Nlight("abababcdcdcd", "nlight_nb_4k4r", 4)
     nlight1.start()
     print("1 started")
-    nligh2 = Virtual_CSRO_Nlight("abababcdcdce", "nlight_nb_4k4r", 2)
+    nligh2 = Virtual_CSRO_Nlight("abababcdcdce", "nlight_nb_4k4r", 4)
     nligh2.start()
     print("2 started")
